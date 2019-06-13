07f8947d5c69d1675aa6da3d35a55c6cf2563a9d
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-06-13 18:47:36 +0530

    Updates

---
+++ precache-manifest.ff29d06ddbfcf58d96656dce0e1fecce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7a9b60580a28a78a368e2c90b1cc104",
+    "url": "/react-github-buttons/index.html"
+  },
+  {
+    "revision": "9da8ec6306a04c7db90f",
+    "url": "/react-github-buttons/static/css/main.7a59bd4a.chunk.css"
+  },
+  {
+    "revision": "e9fb69f3bd20e1e3ae49",
+    "url": "/react-github-buttons/static/js/2.e5f6197d.chunk.js"
+  },
+  {
+    "revision": "9da8ec6306a04c7db90f",
+    "url": "/react-github-buttons/static/js/main.8a6f8c78.chunk.js"
+  },
+  {
+    "revision": "0dd72e3439b968d50570",
+    "url": "/react-github-buttons/static/js/runtime~main.6fb64d52.js"
+  }
+]);
