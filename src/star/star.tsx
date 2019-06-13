092a653fa8952ffee7b9a0a6c1c0bbfd8a986255
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-06-13 16:12:58 +0530

    make star button match github style

--- src/star/star.tsx
+++ src/star/star.tsx
@@ -2,23 +2,23 @@
  * @function Star
  */
 import * as React from "react";
-import { useState } from "react";
-import { ReactComponent as StarSvg } from './star.svg';
+import styles from "../styles.css";
+import StarSvg from "./starIcon";
 
-export type propTypes = { repo: string };
+export interface IPropTypes { repo: string; }
 
-export default function Star(props: propTypes) {
+export default function Star(props: IPropTypes) {
   const { repo } = props;
   return (
-    <div>
-      <button title={`Star ${repo}`}>
+    <div className={styles.root}>
+      <button title={`Star ${repo}`} className={styles.button}>
         <StarSvg />
         Star
       </button>
       <a
-        className="social-count js-social-count"
+        className={styles.count}
         href={`https://github.com/${repo}/stargazers`}
-      aria-label={`${1} user starred this repository`}
+        aria-label={`${1} user starred this repository`}
         target="_blank"
         rel="noreferrer noopener"
       >
